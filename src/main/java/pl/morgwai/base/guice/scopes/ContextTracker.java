3509db0b499e0b532f0ab52b417e682c5e0beb35
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2021-04-19 12:40:05 +0700

    merge ThreadLocalContextTracker into ContextTracker

--- src/main/java/pl/morgwai/base/guice/scopes/ContextTracker.java
+++ src/main/java/pl/morgwai/base/guice/scopes/ContextTracker.java
@@ -8,24 +8,36 @@ package pl.morgwai.base.guice.scopes;
 /**
  * Allows to track which server-side call is handled by which thread.
  */
-public abstract class ContextTracker<Ctx extends ServerSideContext<Ctx>> {
+public class ContextTracker<Ctx extends ServerSideContext<Ctx>> {
+
+
+
+	private final ThreadLocal<Ctx> contexts = new ThreadLocal<>();
+
+
 
 	/**
 	 * @return call context of the calling thread.
 	 */
-	public abstract Ctx getCurrentContext();
+	public Ctx getCurrentContext() {
+		return contexts.get();
+	}
 
 	/**
 	 * For internal use. Apps and deriving libs should rather use
 	 * {@link ServerSideContext#runWithinSelf(Runnable)} and
 	 * {@link ServerSideContext#callWithinSelf(java.util.concurrent.Callable)}.
 	 */
-	protected abstract void setCurrentContext(Ctx ctx);
+	void setCurrentContext(Ctx ctx) {
+		contexts.set(ctx);
+	}
 
 	/**
 	 * For internal use. Apps and deriving libs should rather use
 	 * {@link ServerSideContext#runWithinSelf(Runnable)} and
 	 * {@link ServerSideContext#callWithinSelf(java.util.concurrent.Callable)}.
 	 */
-	protected abstract void clearCurrentContext();
+	protected void clearCurrentContext() {
+		contexts.remove();
+	}
 }
