1b84c6af10bbe055390b805391901219e1940409
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2023-11-23 13:24:43 +0100

    InjectionContextTests: testSerialization(): add serializable objects with refs to non-serializable ones

--- src/test/java/pl/morgwai/base/guice/scopes/InjectionContextTests.java
+++ src/test/java/pl/morgwai/base/guice/scopes/InjectionContextTests.java
@@ -9,12 +9,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import com.google.inject.*;
 import com.google.inject.name.Named;
-import com.google.inject.name.Names;
 import org.junit.Test;
 
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import static com.google.inject.name.Names.named;
 import static org.junit.Assert.*;
 
 
@@ -65,9 +65,24 @@ public class InjectionContextTests {
 
 
 	public static class SerializableNamedObject implements Serializable {
+
 		final String name;
-		SerializableNamedObject(String name) { this.name = name; }
-		private static final long serialVersionUID = -1453519357553231861L;
+		final Object ref;
+
+		SerializableNamedObject(String name) {
+			this(name, null);
+		}
+
+		SerializableNamedObject(String name, Object toRef) {
+			this.name = name;
+			this.ref = toRef;
+		}
+
+		@Override public String toString() {
+			return "SerializableNamedObject { name = \"" + name + "\", ref = " + ref + " }";
+		}
+
+		private static final long serialVersionUID = -750527283388014106L;
 	}
 
 
@@ -75,6 +90,9 @@ public class InjectionContextTests {
 	public static class NonSerializableNamedObject {
 		final String name;
 		NonSerializableNamedObject(String name) { this.name = name; }
+		@Override public String toString() {
+			return "NonSerializableNamedObject { name=\"" + name + "\" }";
+		}
 	}
 
 
@@ -102,10 +120,43 @@ public class InjectionContextTests {
 
 	public void testSerialization(boolean checkIdempotence)
 			throws IOException, ClassNotFoundException, NoSuchFieldException {
+
+		final var nonSerializableObject = new NonSerializableNamedObject("nonSerializableObject");
+		ctx.produceIfAbsent(Key.get(NonSerializableNamedObject.class), () -> nonSerializableObject);
+		final var objectWithRefToNonSerializable1Name = "objectWithRefToNonSerializable1";
+		final var objectWithRefToNonSerializable1 = new SerializableNamedObject(
+				objectWithRefToNonSerializable1Name, nonSerializableObject);
+		final var objectWithRefToNonSerializable1Key =
+				Key.get(SerializableNamedObject.class, named(objectWithRefToNonSerializable1Name));
+		ctx.produceIfAbsent(
+				objectWithRefToNonSerializable1Key, () -> objectWithRefToNonSerializable1);
+
 		final var serializableObject = new SerializableNamedObject(namedString);
+		final var objectWithRefToAnotherScopedSerializableName =
+				"objectWithRefToAnotherScopedSerializable";
+		final var objectWithRefToAnotherScopedSerializable = new SerializableNamedObject(
+				objectWithRefToAnotherScopedSerializableName, serializableObject);
+		final var objectWithRefToAnotherScopedSerializableKey = Key.get(
+				SerializableNamedObject.class, named(objectWithRefToAnotherScopedSerializableName));
+		ctx.produceIfAbsent(
+				Key.get(String.class, named(STRING_NAME)), () -> namedString);
+		ctx.produceIfAbsent(
+				Key.get(SerializableNamedObject.class), () -> serializableObject);
+		ctx.produceIfAbsent(
+			objectWithRefToAnotherScopedSerializableKey,
+			() -> objectWithRefToAnotherScopedSerializable
+		);
+
+		final var objectWithRefToNonSerializable2Name = "objectWithRefToNonSerializable2";
+		final var objectWithRefToNonSerializable2 = new SerializableNamedObject(
+				objectWithRefToNonSerializable2Name, nonSerializableObject);
+		final var objectWithRefToNonSerializable2Key =
+				Key.get(SerializableNamedObject.class, named(objectWithRefToNonSerializable2Name));
+		ctx.produceIfAbsent(
+				objectWithRefToNonSerializable2Key, () -> objectWithRefToNonSerializable2);
+
 		final var theChosenNumber = Integer.valueOf(666);
 		final var anotherNumber = Integer.valueOf(13);
-		final var nonSerializableObject = new NonSerializableNamedObject("nonSerializableObject");
 		final var listOfInts = new ArrayList<Integer>(1);
 		listOfInts.add(theChosenNumber);
 		final TypeLiteral<List<Integer>> listType = new TypeLiteral<>() {};
@@ -113,18 +164,20 @@ public class InjectionContextTests {
 		final TypeLiteral<Integer[]> arrayType = new TypeLiteral<>() {};
 		final String anotherString = "anotherString";
 		ctx.produceIfAbsent(
-				Key.get(SerializableNamedObject.class), () -> serializableObject);
-		ctx.produceIfAbsent(
-				Key.get(String.class, Names.named(STRING_NAME)), () -> namedString);
-		ctx.produceIfAbsent(
 				Key.get(String.class, TheChosenOne.class), () -> theChosenString);
 		ctx.produceIfAbsent(
 				Key.get(Integer.class, new TheChosenOneImpl()), () -> theChosenNumber);
-		ctx.produceIfAbsent(
-				Key.get(NonSerializableNamedObject.class), () -> nonSerializableObject);
 		ctx.produceIfAbsent(Key.get(listType), () -> listOfInts);
 		ctx.produceIfAbsent(Key.get(arrayType), () -> arrayOfInts);
 		ctx.produceIfAbsent(Key.get(Long.class), () -> null);
+
+		final var objectWithRefToNonSerializable3Name = "objectWithRefToNonSerializable3";
+		final var objectWithRefToNonSerializable3 = new SerializableNamedObject(
+			objectWithRefToNonSerializable3Name, nonSerializableObject);
+		final var objectWithRefToNonSerializable3Key =
+			Key.get(SerializableNamedObject.class, named(objectWithRefToNonSerializable3Name));
+		ctx.produceIfAbsent(
+			objectWithRefToNonSerializable3Key, () -> objectWithRefToNonSerializable3);
 
 		if (checkIdempotence) ctx.prepareForSerialization();
 		final var serializedBytesOutput = new ByteArrayOutputStream(500);
@@ -144,6 +197,39 @@ public class InjectionContextTests {
 		}
 		if (checkIdempotence) ctx.restoreAfterDeserialization();
 
+		assertNotEquals(
+			"nonSerializableObject should NOT (de)serialize",
+			nonSerializableObject.name,
+			deserializedCtx.produceIfAbsent(
+				Key.get(NonSerializableNamedObject.class),
+				() -> new NonSerializableNamedObject(anotherString)
+			).name
+		);
+		assertNotEquals(
+			"objectWithRefToNonSerializable1 should NOT (de)serialize",
+			objectWithRefToNonSerializable1Name,
+			deserializedCtx.produceIfAbsent(
+				objectWithRefToNonSerializable1Key,
+				() -> new SerializableNamedObject(anotherString)
+			).name
+		);
+		assertNotEquals(
+			"objectWithRefToNonSerializable2 should NOT (de)serialize",
+			objectWithRefToNonSerializable2Name,
+			deserializedCtx.produceIfAbsent(
+				objectWithRefToNonSerializable2Key,
+				() -> new SerializableNamedObject(anotherString)
+			).name
+		);
+		assertNotEquals(
+			"objectWithRefToNonSerializable3 should NOT (de)serialize",
+			objectWithRefToNonSerializable3Name,
+			deserializedCtx.produceIfAbsent(
+				objectWithRefToNonSerializable3Key,
+				() -> new SerializableNamedObject(anotherString)
+			).name
+		);
+
 		assertEquals(
 			"unannotated serializableObject should (de)serialize",
 			serializableObject.name,
@@ -156,7 +242,7 @@ public class InjectionContextTests {
 			"namedString annotated with @Named(stringName) should (de)serialize",
 			namedString,
 			deserializedCtx.produceIfAbsent(
-					Key.get(String.class, Names.named(STRING_NAME)), () -> anotherString)
+					Key.get(String.class, named(STRING_NAME)), () -> anotherString)
 		);
 		final Named namedReflectiveAnnotation =
 				InjectionContextTests.class.getDeclaredField(namedString)
@@ -167,6 +253,38 @@ public class InjectionContextTests {
 			deserializedCtx.produceIfAbsent(
 					Key.get(String.class, namedReflectiveAnnotation), () -> anotherString)
 		);
+		assertSame(
+			"deserialized name of serializableObject and namedString should be the same object",
+			deserializedCtx.produceIfAbsent(
+				Key.get(SerializableNamedObject.class),
+				() -> new SerializableNamedObject(anotherString)
+			).name,
+			deserializedCtx.produceIfAbsent(
+				Key.get(String.class, named(STRING_NAME)),
+				() -> "yetAnotherString"
+			)
+		);
+		assertEquals(
+			"objectWithRefToAnotherScopedSerializable should (de)serialize",
+			objectWithRefToAnotherScopedSerializableName,
+			deserializedCtx.produceIfAbsent(
+				objectWithRefToAnotherScopedSerializableKey,
+				() -> new SerializableNamedObject(anotherString)
+			).name
+		);
+		assertSame(
+			"deserialized ref of objectWithRefToAnotherScopedSerializable and deserialized "
+					+ "serializableObject should be the same object",
+			deserializedCtx.produceIfAbsent(
+				Key.get(SerializableNamedObject.class),
+				() -> new SerializableNamedObject(anotherString)
+			),
+			deserializedCtx.produceIfAbsent(
+				objectWithRefToAnotherScopedSerializableKey,
+				() -> new SerializableNamedObject(anotherString)
+			).ref
+		);
+
 		assertEquals(
 			"theChosenString annotated with @TheChosenOne should (de)serialize",
 			theChosenString,
@@ -181,7 +299,7 @@ public class InjectionContextTests {
 		);
 		final TheChosenOne theChosenOneReflectiveAnnotation =
 				InjectionContextTests.class.getDeclaredField(theChosenString)
-					.getAnnotation(TheChosenOne.class);
+						.getAnnotation(TheChosenOne.class);
 		assertEquals(
 			"theChosenString should be obtainable by @TheChosenOne reflective instance",
 			theChosenString,
@@ -193,14 +311,6 @@ public class InjectionContextTests {
 			theChosenNumber,
 			deserializedCtx.produceIfAbsent(
 					Key.get(Integer.class, TheChosenOne.class), () -> anotherNumber)
-		);
-		assertNotEquals(
-			"nonSerializableObject should NOT (de)serialize",
-			nonSerializableObject.name,
-			deserializedCtx.produceIfAbsent(
-				Key.get(NonSerializableNamedObject.class),
-				() -> new NonSerializableNamedObject(anotherString)
-			).name
 		);
 		assertEquals(
 			"listOfInts should (de)serialize and have the same size",
@@ -222,17 +332,6 @@ public class InjectionContextTests {
 			"arrayOfInts should (de)serialize and have the same content",
 			arrayOfInts[0],
 			deserializedCtx.produceIfAbsent(Key.get(arrayType), () -> new Integer[1])[0]
-		);
-		assertSame(
-			"deserialized name of serializableObject and namedString should be the same object",
-			deserializedCtx.produceIfAbsent(
-				Key.get(SerializableNamedObject.class),
-				() -> new SerializableNamedObject(anotherString)
-			).name,
-			deserializedCtx.produceIfAbsent(
-				Key.get(String.class, Names.named(STRING_NAME)),
-				() -> "yetAnotherString"
-			)
 		);
 		assertNull("a Key bound to null should retain null",
 				deserializedCtx.produceIfAbsent(Key.get(Long.class), () -> 666L));
