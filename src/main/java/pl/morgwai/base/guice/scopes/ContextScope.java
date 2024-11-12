2967aeade68b8258a552ae45e097f1df1da8754b
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2024-11-12 12:06:27 +0100

    ContextScope: readability: make ScopedProvider anonymous again

--- src/main/java/pl/morgwai/base/guice/scopes/ContextScope.java
+++ src/main/java/pl/morgwai/base/guice/scopes/ContextScope.java
@@ -15,17 +15,17 @@ import com.google.inject.*;
  * processing of HTTP requests.<br/>
  * By default a {@code ContextScope} instance is associated with {@code ContextT} class and its
  * instances are {@link #getCurrentContext()  obtained} directly from the associated
- * {@link ContextTracker} passed via {@link #ContextScope(String, ContextTracker) the contructor}.
- * </p>
+ * {@link #tracker ContextTracker}.</p>
  * <p>
- * {@code  ContextScope} instances are usually created at an app startup to be used in bindings in
- * user {@link com.google.inject.Module}s. See {@link ScopeModule} for details.</p>
+ * {@code  ContextScope} instances are usually created at the startup of an app to be used in
+ * bindings in user {@link com.google.inject.Module}s.</p>
+ * @see ScopeModule
  */
 public class ContextScope<ContextT extends TrackableContext<? super ContextT>> implements Scope {
 
 
 
-	/** For obtaining the current {@code ContextT} instance. */
+	/** The associated {@link ContextTracker} for obtaining {@code ContextT} instances. */
 	public final ContextTracker<ContextT> tracker;
 	public ContextTracker<ContextT> getTracker() { return tracker; }
 
@@ -42,74 +42,52 @@ public class ContextScope<ContextT extends TrackableContext<? super ContextT>> i
 
 
 
-	/** Wraps {@code producer} with a {@link ScopedProvider}. */
-	@Override
-	public <T> ScopedProvider<T> scope(Key<T> key, Provider<T> producer) {
-		return new ScopedProvider<>(key, producer);
-	}
-
-
-
 	/**
-	 * Returned by {@link #scope(Key, Provider)}, provides objects scoped to the
-	 * {@link #getCurrentContext() Context current} at the moment of a given
-	 * {@link #get() provisioning}.
+	 * Scopes {@code Object}s to the {@code Context} returned by {@link #getCurrentContext()} at the
+	 * moment of a given {@link Provider#get() provisioning}.
+	 * <p>
+	 * The returned scoped {@link Provider} will throw an {@link OutOfScopeException} if its called
+	 * in a {@code Thread} running outside of any {@code Context} of {@code ContextT} type
+	 * associated with this {@link ContextScope}.<br/>
+	 * This most commonly happens if some async task was not
+	 * {@link ContextBinder#bindToContext(Runnable) bound to its current Context} before being
+	 * dispatched to this {@code Thread}. Use {@link ContextTrackingExecutor} to transfer
+	 * {@code Context}s automatically when passing tasks to {@link java.util.concurrent.Executor}s
+	 * or bind manually using {@link ContextBinder}.</p>
 	 */
-	public class ScopedProvider<T> implements Provider<T> {
-
-		final Key<T> key;
-		final Provider<T> producer;
-
-
-
-		ScopedProvider(Key<T> key, Provider<T> producer) {
-			this.key = key;
-			this.producer = producer;
-		}
-
-
-
-		/**
-		 * Provides an {@code Object} scoped to the {@code Context} returned by a call to
-		 * {@link #getCurrentContext()}.
-		 * @throws OutOfScopeException if the current {@code Thread} is running outside of any
-		 *     {@code Context} of {@code ContextT} type associated with this {@link ContextScope}.
-		 *     This most commonly happens if some async task was not
-		 *     {@link ContextBinder#bindToContext(Runnable) bound to its current Context} before
-		 *     being dispatched to this {@code Thread}. Use {@link ContextTrackingExecutor}
-		 *     to transfer {@code Context}s automatically when passing tasks to
-		 *     {@link java.util.concurrent.Executor}s or bind manually using {@link ContextBinder}.
-		 */
-		@Override public T get() {
-			try {
-				return getCurrentContext().produceIfAbsent(key, producer);
-			} catch (NullPointerException e) {
-				throw new OutOfScopeException(
-						String.format(NO_CONTEXT_MESSAGE, name, Thread.currentThread().getName()));
+	@Override
+	public <T> Provider<T> scope(Key<T> key, Provider<T> producer) {
+		return new Provider<>() {
+			@Override public T get() {
+				try {
+					return getCurrentContext().produceIfAbsent(key, producer);
+				} catch (NullPointerException e) {
+					throw new OutOfScopeException(String.format(
+							NO_CONTEXT_MESSAGE, name, Thread.currentThread().getName()));
+				}
 			}
-		}
-
-		static final String NO_CONTEXT_MESSAGE = "no Context of Scope \"%s\" in Thread \"%s\": "
-				+ "see the javadoc for ContextScope.ScopedProvider.get() -> "
-				+ "https://javadoc.io/doc/pl.morgwai.base/guice-context-scopes/latest/pl/morgwai/"
-				+ "base/guice/scopes/ContextScope.ScopedProvider.html#get()";
-
-
-
-		@Override public String toString() {
-			return "ScopedProvider { scope = \"" + name + "\", key = " + key + ", producer = "
-					+ producer + " }";
-		}
+			@Override public String toString() {
+				return "ScopedProvider { scope = \"" + name + "\", key = " + key + ", producer = "
+						+ producer + " }";
+			}
+		};
 	}
+
+	static final String NO_CONTEXT_MESSAGE = "no Context of Scope \"%s\" in Thread \"%s\": "
+			+ "see the javadoc for ContextScope.scope(...) -> "
+			+ "https://javadoc.io/doc/pl.morgwai.base/guice-context-scopes/latest"
+			+ "/pl/morgwai/base/guice/scopes/ContextScope.html"
+			+ "#scope(com.google.inject.Key,com.google.inject.Provider)";
 
 
 
 	/**
-	 * Returns the {@code Context} from which a scoped {@code Object} should be obtained.
-	 * Called during each {@link ScopedProvider#get() provisioning}. By default returns a
-	 * {@code Context} {@link ContextTracker#getCurrentContext() obtained} directly from
-	 * {@link #tracker}. May be overridden for example to return some {@code Context} induced by the
-	 * one from {@link #tracker} (see {@link InducedContextScope}).
+	 * Returns the {@link InjectionContext Context} from which a scoped {@code Object} should be
+	 * obtained.
+	 * Called during each {@link Provider#get() provisioning}. By default returns a {@code ContextT}
+	 * {@link ContextTracker#getCurrentContext() obtained} directly from {@link #tracker}. May be
+	 * overridden for example to return some {@code Context} induced by {@code ContextT} (see
+	 * {@link InducedContextScope}).
 	 */
 	protected InjectionContext getCurrentContext() {
 		return tracker.getCurrentContext();
